6aa8c7cdfd4370035cce20517a920ccfb786d682
EvaKorsya <korsaeva24@gmail.com>
2023-11-03 19:54:47 +0400

    added default and random constructors, overloaded operators [] and <<

--- include/list/list.h
+++ include/list/list.h
@@ -1,7 +1,70 @@
 #include <random>
 #include <stdexcept>
-
+#include <iostream>
 template<class T>
-class list {
+class linked_list {
+	struct node {
+		T value;
+		node* prev;
+		node* next;
 
+		node(T value, node* prev, node* next) : value(value), prev(prev), next(next) {};
+	};
+	node* _head;
+	size_t _size;
+public:
+	linked_list() : _head(new node{0,nullptr,nullptr}), _size(0) {};
+	linked_list(size_t size) : _size(size) {
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution<> dist_for_head(1, 9);
+		std::uniform_int_distribution<> dist(0, 9);
+		_head = new node(dist_for_head(gen), nullptr, nullptr);
+		auto tmp = _head;
+		size_t s = 0;
+		while (s < size) {
+			tmp->next = new node(dist(gen), tmp, nullptr);
+			tmp = tmp->next;
+			++s;
+		}
+	}
+	const node& operator[](size_t index) const {
+		if (index < 0 || index >= _size) throw std::invalid_argument("index out of the range");
+		size_t cur_ind = 0;
+		node* tmp = _head;
+		while (cur_ind++!=index) {
+			tmp = tmp->next;
+		}
+		return *tmp;
+	}
+	node& operator[](size_t index){
+		if (index < 0 || index >= _size) throw std::invalid_argument("index out of the range");
+		size_t cur_ind = 0;
+		node* tmp = _head;
+		while (cur_ind++ != index) {
+			tmp = tmp->next;
+		}
+		return *tmp;
+	}
+	friend std::ostream& operator<<(std::ostream& os, const linked_list& list) {
+		os << "{ ";
+		for (size_t i = 0; i < list._size; ++i) {
+			os << list[i].value << " ";
+		}
+		os << "}";
+		return os;
+	}
+	~linked_list() {
+		while (_head) {
+			auto tmp = _head;
+			_head = _head->next;
+			delete tmp;
+		}
+	}
+	size_t get_size() {
+		return _size;
+	}
+	node* get_head() {
+		return _head;
+	}
 };
--- tests/list_tests.cc
+++ tests/list_tests.cc
@@ -2,6 +2,22 @@
 #include <gtest/gtest.h>
 #include <iostream>
 //1
-TEST(list_tests,constructor_params) {
-	ASSERT_TRUE(true);
+TEST(list_tests,default_constructor) {
+	linked_list<int> list = linked_list<int>();
+	ASSERT_EQ(list.get_size(), 0);
+	ASSERT_EQ(list.get_head()->value, 0);
+}
+//2
+TEST(list_tests, random_value_constructor) {
+	linked_list<int> list = linked_list<int>(3);
+	ASSERT_EQ(list.get_size(), 3);
+}
+//3
+TEST(list_tests, overloaded_brackets) {
+	linked_list<int> list = linked_list<int>(3);
+	std::cout << list;
+	std::cout << list[0].value;
+	std::cout << list[1].value;
+	std::cout << list[2].value;
+	//ASSERT_EQ(list.get_head()->value, 0);
 }
