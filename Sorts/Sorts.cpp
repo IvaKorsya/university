9c5d43af2c1dcec33eefc02b1397e3d5fc763def
Iva Korsya <korsaeva24@gmail.com>
2023-12-04 15:04:22 +0400

    added bubble sort

--- Sorts/Sorts.cpp
+++ Sorts/Sorts.cpp
@@ -1,6 +1,28 @@
 ﻿#include <iostream>
+#include <vector>
+
+template<class Iter>
+void bubble_sort(Iter begin, Iter end) {
+    bool swapped;
+    if ((end-begin) <= 1) return;
+    do {
+        swapped = false;
+        for (auto i = begin, i_next = std::next(begin); i_next != end; ++i, ++i_next) {
+            if (*i > *i_next) {
+                std::swap(*i, *i_next);
+                swapped = true;
+            }
+            
+        }
+        
+    } while (swapped);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector<int> vect = { 99, 5, 7, 3 };
+    bubble_sort(vect.begin(), vect.end());
+    for (size_t i = 0; i < vect.size(); ++i) {
+        std::cout << vect[i];
+    }   
 }
